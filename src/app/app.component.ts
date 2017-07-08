29911bca05201fc326e8458e0a7debc615ba13eb
Ronald Wong <ronald.acer82@gmail.com>
2017-07-07 21:38:16 -0600

    two way binding using formmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+	  <h1>{{title}}</h1>
+	  <h2>{{hero.name}} details!</h2>
+	  <div><label>id: </label>{{hero.id}}</div>
+	 	<div>
+		  <label>name: </label>
+		  <input [(ngModel)]="hero.name" placeholder="name">
+		</div>
+	  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Time Tracker App';
+  hero: Hero = {
+	  id: 1,
+	  name: 'Windstorm'
+	};
 }
